cd11028b5130a2cbb1f6fcccf5594ba03103c3ec
David mattei <d.mattei89@gmail.com>
2021-10-05 19:34:42 +0200

    chore: setup test

---
+++ tests/Unit/TestTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class TestTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertSame(true, true);
+    }
+}
